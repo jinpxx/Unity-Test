da6b80342908fd4ab0f2fd6c530983078bd7fb6d
jinpxx <jinpxx_young@163.com>
2023-06-24 16:01:35 +0800

    first change
    It's just debug.log("Hello Github!")

---
+++ UnityTest/Assets/Scripts/JustUpAndDown.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JustUpAndDown : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello Github!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
